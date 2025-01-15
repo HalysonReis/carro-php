e956579ae5162a65818e1650b82205df00bf7d80
Halyson Reis <halysondasilvadosreis@gmail.com>
2025-01-14 22:19:19 -0300

    carro

---
+++ Carro.php
@@ -0,0 +1,16 @@
+<?php
+
+class Carro {
+    private int $num_chassi;
+    private string $ano_carro;
+    private string $dono;
+    public float $preco;
+    public string $cor;
+    public int $tamanho_aro;
+    public string $marca;
+    public string $modelo;
+    public int $qnt_portas;
+    public int $km_rodados;
+}
+
+?>
